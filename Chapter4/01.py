22aad44e0fe39979c53a34513ffe1b4d74c7d909
jcarlos21 <j.carlos2020@live.com>
2024-04-21 20:17:17 -0300

    Chapter4

--- Chapter4/01.py
+++ Chapter4/01.py
@@ -1,6 +1,6 @@
 """
-Leia um número inteiro e o imprima.
+Question 1: Leia um número inteiro e o imprima.
 """
 
-numero_inteiro = int(input("Entre com um núermo inteiro: "))
+numero_inteiro = int(input("Digite um núermo inteiro: "))
 print(f"O número digitado foi: {numero_inteiro}")
---
+++ Chapter4/02.py
@@ -0,0 +1,6 @@
+"""
+Question 2: Leia um número real e o imprima.
+"""
+
+numero_real = float(input("Digite um número real: "))
+print(f"O número digitado foi: {numero_real}")
