fa51ca245a09d32cb508e8415750d74f093d0f0c
jcarlos21 <j.carlos2020@live.com>
2024-04-21 20:19:18 -0300

    Chapter4

--- Chapter4/01.py
+++ Chapter4/01.py
@@ -2,5 +2,5 @@
 Question 1: Leia um número inteiro e o imprima.
 """
 
-numero_inteiro = int(input("Digite um núermo inteiro: "))
+numero_inteiro = int(input("Digite um número inteiro: "))
 print(f"O número digitado foi: {numero_inteiro}")
