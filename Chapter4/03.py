4c0c717cddbdd9050d164bc7f0753d516b620da6
jcarlos21 <j.carlos2020@live.com>
2024-05-01 08:54:11 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/03.py
@@ -0,0 +1,9 @@
+"""
+Question 3:
+"""
+
+valor1 = int(input("Digite o primeiro valor inteiro: "))
+valor2 = int(input("Digite o segundo valor inteiro: "))
+valor3 = int(input("Digite o terceiro valor inteiro: "))
+
+print(f'A soma dos valores {valor1}, {valor2} e {valor3} é: {valor1 + valor2 + valor3}')
---
+++ Chapter4/04.py
@@ -0,0 +1,6 @@
+"""
+Question 4:
+"""
+
+valor_real = float(input("Entre com um número real: "))
+print(f'O quadrado do número é: {valor_real ** 2}')
