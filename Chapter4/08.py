fefe910fa18a5e8502329e77548d704848d0f779
jcarlos21 <j.carlos2020@live.com>
2024-05-02 00:06:23 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/08.py
+++ Chapter4/08.py
@@ -6,4 +6,4 @@ temp_graus_kelvin = float(input('Entre com uma temperatura em graus Kelvin: '))
 
 temp_graus_celsius = temp_graus_kelvin - 273.15
 
-print(f'A temperatura {temp_graus_kelvin}ºK em graus Celsius é: {temp_graus_celsius.__round__(2)}ºC')
+print(f'A temperatura {temp_graus_kelvin}K em graus Celsius é: {temp_graus_celsius.__round__(2)}ºC')
---
+++ Chapter4/09.py
@@ -0,0 +1,9 @@
+"""
+Question 9:
+"""
+
+temp_graus_celsius = float(input('Entre com uma temperatura em graus Celsius: '))
+
+temp_graus_kelvin = temp_graus_celsius + 273.15
+
+print(f'A temperatura {temp_graus_celsius}ºC em graus Kelvin é: {temp_graus_kelvin.__round__(2)}K')
