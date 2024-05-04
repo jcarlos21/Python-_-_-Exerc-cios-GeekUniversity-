3d61e2942906db2e72296efa0fb3b8d46598c8b2
jcarlos21 <j.carlos2020@live.com>
2024-05-03 23:35:54 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/16.py
@@ -0,0 +1,9 @@
+"""
+Question 16:
+"""
+
+comprimento_polegada = float(input('Entre com um comprimento em polegadas: '))
+
+comprimento_centimetro = comprimento_polegada * 2.54
+
+print(f'O comprimento {comprimento_polegada} polegadas em centímetro é: {comprimento_centimetro.__round__(2)} cm')
---
+++ Chapter4/17.py
@@ -0,0 +1,9 @@
+"""
+Question 17:
+"""
+
+comprimento_centimetro = float(input('Entre com um comprimento em centimetro: '))
+
+comprimento_polegada = comprimento_centimetro / 2.54
+
+print(f'O comprimento {comprimento_centimetro}cm em polegadas é: {comprimento_polegada.__round__(2)}"')
