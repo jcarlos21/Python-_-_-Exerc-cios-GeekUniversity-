6010db50ac89d1539597fe13aa0d9c812d8b3e64
jcarlos21 <j.carlos2020@live.com>
2024-05-03 23:56:11 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/20.py
+++ Chapter4/20.py
@@ -6,5 +6,5 @@ massa_kg = float(input('Entre com uma massa em kilogramas: '))
 
 massa_libra = massa_kg / 0.45
 
-print(f'A massa {massa_kg} kg em libras é: {massa_libra.__round__(2)} libras')
+print(f'A massa {massa_kg} kilogramas em libras é: {massa_libra.__round__(2)} libras')
 
---
+++ Chapter4/21.py
@@ -0,0 +1,10 @@
+"""
+Question 21:
+"""
+
+massa_libra = float(input('Entre com uma massa em libras: '))
+
+massa_kg = massa_libra * 0.45
+
+print(f'A massa {massa_libra} libras em kilogramas (kg) é: {massa_kg.__round__(2)} kg')
+
