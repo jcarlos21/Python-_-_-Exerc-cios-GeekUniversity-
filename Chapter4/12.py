d0cbb3b00b5d1e6a66858c2b1c5a0f90e1eb42c5
jcarlos21 <j.carlos2020@live.com>
2024-05-03 23:08:30 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/12.py
@@ -0,0 +1,9 @@
+"""
+Question 12:
+"""
+
+distancia_milha = float(input('Entre com uma distância em milhas: '))
+
+distancia_km = 1.61 * distancia_milha
+
+print(f'A distância {distancia_milha} milhas em kilometros é: {distancia_km.__round__(2)} km')
---
+++ Chapter4/13.py
@@ -0,0 +1,9 @@
+"""
+Question 13:
+"""
+
+distancia_km = float(input('Entre com uma distância em kilometros: '))
+
+distancia_milha = distancia_km / 1.61
+
+print(f'A distância {distancia_km} kilometros em milhas é: {distancia_milha.__round__(2)} km')
