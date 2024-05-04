19558ab99a6d81f45f0d41f479ad7475c664bfae
jcarlos21 <j.carlos2020@live.com>
2024-05-03 23:11:20 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/13.py
+++ Chapter4/13.py
@@ -6,4 +6,4 @@ distancia_km = float(input('Entre com uma distância em kilometros: '))
 
 distancia_milha = distancia_km / 1.61
 
-print(f'A distância {distancia_km} kilometros em milhas é: {distancia_milha.__round__(2)} km')
+print(f'A distância {distancia_km} kilometros em milhas é: {distancia_milha.__round__(2)} milhas')
