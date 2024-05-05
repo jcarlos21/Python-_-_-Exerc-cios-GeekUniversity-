9560a73de5b539ed26a3a090594fb49cfada4931
jcarlos21 <j.carlos2020@live.com>
2024-05-05 20:22:51 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/22.py
@@ -0,0 +1,9 @@
+"""
+Question 22:
+"""
+
+comprimento_jardas = float(input('Etnre com um comprimento em jardas: '))
+
+comprimento_metros = 0.91 * comprimento_jardas
+
+print(f'O comprimento {comprimento_jardas} jardas em metros é: {comprimento_metros.__round__(2)} metros')
