c7b768f95ca5898172dce2508383e003fb280731
jcarlos21 <j.carlos2020@live.com>
2024-05-05 21:29:58 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/24.py
+++ Chapter4/24.py
@@ -6,4 +6,4 @@ area_m2 = float(input('Entre com uma área em m²: '))
 
 area_acres = area_m2 * 0.000247
 
-print(f'A área {area_m2} m² em acres é: {area_acres.__round__(2)} acres')
+print(f'A área {area_m2}m² em acres é: {area_acres.__round__(2)} acres')
---
+++ Chapter4/25.py
@@ -0,0 +1,9 @@
+"""
+Question 25
+"""
+
+area_acres = float(input('Entre com uma área em acres: '))
+
+area_m2 = area_acres * 4048.58
+
+print(f'A área {area_acres} acres em m² é: {area_m2.__round__(2)}m² ')
