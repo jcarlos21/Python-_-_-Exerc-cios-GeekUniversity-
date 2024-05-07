29b0b1a32c2d4c0ead7818cdcd291b27b9f2d429
jcarlos21 <j.carlos2020@live.com>
2024-05-06 22:06:23 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/32.py
@@ -0,0 +1,12 @@
+"""
+Question 32
+"""
+
+num_inteiro = int(input('Entre com um número inteiro: '))
+
+soma = ((3 * num_inteiro) + 1) + ((2 * num_inteiro) - 1)
+
+print(f'Sucessor do triplo de {num_inteiro}: {(3 * num_inteiro) + 1}')
+print(f'Antecessor do dobro de {num_inteiro}: {(2 * num_inteiro) - 1}')
+
+print(f'A soma do sucessor do triplo de {num_inteiro} com o antecessor do dobro de {num_inteiro} é: {soma}')
