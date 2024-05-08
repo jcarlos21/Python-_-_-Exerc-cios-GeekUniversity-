efa4aed65df0896c0d9a673c4e51ba3d962af3eb
jcarlos21 <j.carlos2020@live.com>
2024-05-07 21:49:44 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/33.py
@@ -0,0 +1,11 @@
+"""
+Question 33
+"""
+
+import math as mt
+
+lado_quadrado = float(input('Entre com o lado de um quadrado: '))
+
+area_quadrado = mt.pow(lado_quadrado, 2)
+
+print(f'A área do quadrado é: {area_quadrado} ')
