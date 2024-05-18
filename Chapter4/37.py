7e8ac3c6355f3175819a4fcf576d2ac41c1cd760
jcarlos21 <j.carlos2020@live.com>
2024-05-17 22:00:06 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/37.py
+++ Chapter4/37.py
@@ -3,7 +3,6 @@ Question 37
 """
 
 valor_produto = float(input('Entre com o valor do produto: '))
-
 valor_produto_desconto = valor_produto - (valor_produto * 0.12)
 
 print(f'O valor do produto com desconto de 12% é: R$ {valor_produto_desconto.__round__(2)}')
