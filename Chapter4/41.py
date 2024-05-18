6adb9a902cac0ee47a5dd89205a9a023097f8ab6
jcarlos21 <j.carlos2020@live.com>
2024-05-18 17:37:38 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/41.py
@@ -0,0 +1,11 @@
+"""
+Question 41
+"""
+
+valor_hora_trabalho = float(input('Entre com o valor da hora de trabalho do funcionário: '))
+
+horas_trabalhadas = float(input('Entre com o número de horas trabalhadas: '))
+
+valor_a_ser_pago = (valor_hora_trabalho * horas_trabalhadas) * 1.10
+
+print(f'O valor da hora trabalhada com adição de 10% é: R$ {valor_a_ser_pago.__round__(2)}')
