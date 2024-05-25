64a3402f31c05160d51cc40deabff25a595a57e3
jcarlos21 <j.carlos2020@live.com>
2024-05-24 22:05:34 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/44.py
+++ Chapter4/44.py
@@ -4,7 +4,6 @@ Question 44
 
 altura_degrau_escada = float(input('Entre com a altura do degrau da escada: '))
 altura_desejada = float(input('Entre com a altura desejada: '))
-
 quantidade_degraus = altura_desejada / altura_degrau_escada
 
 print(f'O usuário deverá subir {int(quantidade_degraus)} degraus para atingir a altura desejada.')
