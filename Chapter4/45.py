275e275134c30329ebb06348b089bd0b3f7290b3
jcarlos21 <j.carlos2020@live.com>
2024-05-24 22:45:57 -0300

    Exercícios de Python do capitulo 4

---
+++ Chapter4/45.py
@@ -0,0 +1,9 @@
+"""
+Question 45
+"""
+
+letra = input('Entre com uma letra maiúscula: ')
+
+letra_lowercase = letra.lower()
+
+print(f'A letra {letra} transformada em minúscula fica: {letra_lowercase}')
