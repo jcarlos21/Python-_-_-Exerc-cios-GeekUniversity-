f965c2385814d9721e6dbee6f7f239432b0f7368
jcarlos21 <j.carlos2020@live.com>
2024-05-26 22:07:37 -0300

    Exercícios de Python do capitulo 4

--- Chapter4/46.py
+++ Chapter4/46.py
@@ -5,4 +5,4 @@ Question 46
 num_int = input('Entre com um número inteiro de três dígitos: ')
 num_int_invertido = num_int[::-1]
 
-print(f'O número {num_int} invertido fica: {num_int_invertido}')
+print(f'O número {num_int} de forma inversa fica: {num_int_invertido}')
