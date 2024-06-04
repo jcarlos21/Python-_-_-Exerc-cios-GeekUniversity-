79f8eef3a37a2ee7d652a685635d848a769002a0
jcarlos21 <j.carlos2020@live.com>
2024-06-04 10:11:17 -0300

    Exercícios do Capítulo 4

--- Chapter4/41.py
+++ Chapter4/41.py
@@ -3,7 +3,6 @@ Question 41
 """
 
 valor_hora_trabalho = float(input('Entre com o valor da hora de trabalho do funcionário: '))
-
 horas_trabalhadas = float(input('Entre com o número de horas trabalhadas: '))
 
 valor_a_ser_pago = (valor_hora_trabalho * horas_trabalhadas) * 1.10
