fc3505529c7092e5d3376b6c59d504ddb8acd8d2
jcarlos21 <j.carlos2020@live.com>
2024-06-10 22:04:19 -0300

    Exercícios de Python do Capítulo 4

---
+++ Chapter4/50.py
@@ -0,0 +1,10 @@
+"""
+Question 50
+"""
+
+idade = int(input('Entre com sua idade: '))
+ano_atual = int(input('Entre com o ano atual: '))
+
+ano_de_nascimento = ano_atual - idade
+
+print(f'O ano de seu nascimento é: {ano_de_nascimento}')
